56d7130b08a6e7d4a923412e43a3dac4637507f7
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-28 02:30:52 -0500

    Users App: setup de los tests

---
+++ bookstore_users/tests/factory.py
@@ -0,0 +1,13 @@
+from faker   import Faker as FakerClass
+from factory import Faker, django, post_generation
+
+from bookstore_users.models.user import User
+
+class UserFactory(django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = Faker("first_name")
+    last_name  = Faker("last_name")
+    email      = Faker("email")
+    password   = FakerClass().password()
---
+++ bookstore_users/tests/test_setup.py
@@ -0,0 +1,26 @@
+# Django
+from django.urls import reverse
+# Django REST framework
+from rest_framework.test import APITestCase, APIClient
+# Faker
+from faker import Faker
+# Local
+from bookstore_users.tests.factory import UserFactory
+
+class TestSetUp(APITestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.user_saved_password = "password123"
+        cls.user_saved  = UserFactory.create(
+            password = cls.user_saved_password
+        )
+        cls.user_object = UserFactory.build()
+        cls.faker_obj   = Faker()
+        cls.client      = APIClient()
+
+        cls.login_url   = reverse("token_obtain_pair")
+        cls.refresh_url = reverse("token_refresh")
+        cls.crear_listar_url = reverse("user:get_post")
