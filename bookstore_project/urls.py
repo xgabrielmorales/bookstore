4054098bd5f40d9bde44efdf97764a687a9076ef
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-28 01:44:19 -0500

    Obtención del access y el refresh token

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# D.R.F Simplw JWT
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include("bookstore_users.urls"))
+    path('api/user/', include("bookstore_users.urls")),
+    # Token Management
+    path("api/token/",   TokenObtainPairView.as_view(), name = "token_obtain_pair"),
+    path("api/refresh/", TokenRefreshView.as_view(),    name = "token_refresh"),
 ]
