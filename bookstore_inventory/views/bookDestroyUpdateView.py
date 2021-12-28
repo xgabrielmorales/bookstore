fc99b963b81bcd920207169a4eb47969371b9b18
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-28 03:08:26 -0500

    Inventory App: Vista de Destroy y Update para el API

---
+++ bookstore_inventory/views/bookDestroyUpdateView.py
@@ -0,0 +1,26 @@
+# Django Rest Framework
+from rest_framework             import status
+from rest_framework.response    import Response
+from rest_framework.generics    import GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+# Local
+from bookshop_inventory.models.book import Book
+from bookshop_inventory.serializers import BookSerializer
+
+class BookDestroyUpdateView(GenericAPIView):
+    serializer_class   = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        instance = Book.objects.get(id = kwargs["pk"])
+
+        serializer = self.get_serializer(instance, request.data)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+
+        return Response(serializer.data, status = status.HTTP_200_OK)
+
+    def delete(self, request, *args, **kwargs):
+        instance = Book.objects.get(id = kwargs["pk"])
+        instance.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
