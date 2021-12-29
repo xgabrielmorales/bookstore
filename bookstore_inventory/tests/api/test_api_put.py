e97d0fd37d6f7ad6867b2084ceeffe24bca3a28e
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 01:29:55 -0500

    Inventory App: test del metodo put del modelo book

---
+++ bookstore_inventory/tests/api/test_api_put.py
@@ -0,0 +1,29 @@
+# Django
+from django.urls import reverse
+# Django REST Framework
+from rest_framework import status
+# Local
+from bookstore_inventory.models.book          import Book
+from bookstore_inventory.tests.api.test_setup import TestSetUp
+
+class BookPutTestCase(TestSetUp):
+    def test_replace_book(self):
+        new_book_data = {
+            "title"     : self.faker_obj.color_name(),
+            "author"    : self.faker_obj.name(),
+            "editorial" : "Palabras",
+            "gender"    : "Autobiograficos",
+            "num_pages" : self.faker_obj.random_int(),
+            "pub_date"  : self.faker_obj.date(),
+        }
+
+        response = self.client.put(
+            reverse(
+                "inventory:book_delete_put",
+                kwargs = {"pk": self.book_saved.id}
+            ),
+            data = new_book_data
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["title"], new_book_data["title"])
