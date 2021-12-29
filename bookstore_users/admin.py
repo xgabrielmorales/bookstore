5f8f3933deefed704fe7e2d90862af1ccbd7755b
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 12:27:45 -0500

    Una vista de admin más agradable para los Users

--- bookstore_users/admin.py
+++ bookstore_users/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+class UsuarioAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Nombres",      {"fields": ["first_name", "last_name"]}),
+        ("Credenciales", {"fields": ["email", "password"]}),
+        ("Atributos",    {"fields": ["is_active", "is_staff", "is_superuser"]}),
+        ("Otros",        {"fields": ["last_login"]})
+    ]
+
+    search_fields = ['first_name', "last_name"]
+    list_display = ("__str__", "email")
+
+admin.site.register(User, UsuarioAdmin)
