162c71773639f624ae23b9b6b767d4e39970dc48
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 08:33:50 -0500

    Archivo de configuración segmentado

--- bookstore_project/settings/base.py
+++ bookstore_project/settings/base.py
@@ -4,18 +4,11 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-6%!2r5ky40e_ah%d&b2&r%$%k!ja8d+hix$$j$*86n=7eer#8x'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -63,17 +56,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'bookstore_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 AUTH_USER_MODEL = "bookstore_users.User"
 
