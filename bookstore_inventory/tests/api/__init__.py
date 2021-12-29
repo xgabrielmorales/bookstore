689d1431225076c367aab36b34556166e10aa822
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 01:31:20 -0500

    Inventory App: test __init__

---
+++ bookstore_inventory/tests/api/__init__.py
@@ -0,0 +1,5 @@
+from .test_setup      import TestSetUp
+from .test_api_get    import BookGetTestCase
+from .test_api_post   import BookPostTestCase
+from .test_api_put    import BookPutTestCase
+from .test_api_delete import BookDeleteTestCase
