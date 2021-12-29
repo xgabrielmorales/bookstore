185f6d7757884b152d3904d74b2f3d72f53d232b
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 01:28:25 -0500

    Inventory App: test del metodo post del modelo book

---
+++ bookstore_inventory/tests/api/test_api_get.py
@@ -0,0 +1,12 @@
+# Django
+from django.urls import reverse
+# Django REST Framework
+from rest_framework import status
+# Local
+from bookstore_inventory.models.book          import Book
+from bookstore_inventory.tests.api.test_setup import TestSetUp
+
+class BookGetTestCase(TestSetUp):
+    def test_list_all_books(self):
+        response = self.client.get(reverse("inventory:book_get_post"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
