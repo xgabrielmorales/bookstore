589a8da8f0ae4a7def095d1aed88cf40a85800d1
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 01:30:20 -0500

    Inventory App: Test del metodo delete del modelo book

---
+++ bookstore_inventory/tests/api/test_api_delete.py
@@ -0,0 +1,24 @@
+# Django
+from django.urls import reverse
+# Django REST Framework
+from rest_framework import status
+# Local
+from bookstore_inventory.models.book          import Book
+from bookstore_inventory.tests.api.test_setup import TestSetUp
+
+class BookDeleteTestCase(TestSetUp):
+    def test_delete_book(self):
+
+        response = self.client.delete(
+            reverse(
+                "inventory:book_delete_put",
+                kwargs = {"pk": self.book_saved.id}
+            ),
+        )
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_204_NO_CONTENT
+        )
+
+        self.assertEqual(Book.objects.count(), 0)
