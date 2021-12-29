1f3ddb27d77e8f9b9dfb12ca1803113fe3c249a4
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 12:18:15 -0500

    Campos obligatorios para el superuser

--- bookstore_users/models/user.py
+++ bookstore_users/models/user.py
@@ -19,6 +19,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
 
     def __str__(self):
         return f"{self.first_name}" if not self.last_name \
