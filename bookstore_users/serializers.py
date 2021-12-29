9aca09e45822ea3c78f7696c5bfada04a4db8da8
Gabriel Morales <gabrielmorales1@pm.me>
2021-12-29 07:52:26 -0500

    Update también se debería hasher el password

--- bookstore_users/serializers.py
+++ bookstore_users/serializers.py
@@ -16,3 +16,12 @@ class UserSerializer(ModelSerializer):
         user.save()
 
         return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password")
+
+        user = super().update(instance, validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
